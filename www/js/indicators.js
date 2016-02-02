5a9b304af3a8034c27de967ece31cb480d614855
Marlo Delatorre <delatorm@purdue.edu>
2016-02-02 15:21:00 -0500

    Updated index with indicators and tabs.

---
+++ www/js/indicators.js
@@ -0,0 +1,44 @@
+$('#earhartContainer').radialIndicator({
+        radius: 80,
+        barColor: '#33cc33',
+        barWidth: 10,
+        initValue: 20,
+        roundCorner : false,
+        percentage: true
+    });
+
+$('#windsorContainer').radialIndicator({
+        radius: 80,
+        barColor: '#33cc33',
+        barWidth: 10,
+        initValue: 20,
+        roundCorner : false,
+        percentage: true
+    });
+
+$('#wileyContainer').radialIndicator({
+        radius: 80,
+        barColor: '#33cc33',
+        barWidth: 10,
+        initValue: 20,
+        roundCorner : false,
+        percentage: true
+    });
+
+$('#fordContainer').radialIndicator({
+        radius: 80,
+        barColor: '#33cc33',
+        barWidth: 10,
+        initValue: 20,
+        roundCorner : false,
+        percentage: true
+    });
+
+$('#hillenbrandContainer').radialIndicator({
+        radius: 80,
+        barColor: '#33cc33',
+        barWidth: 10,
+        initValue: 20,
+        roundCorner : false,
+        percentage: true
+    });
