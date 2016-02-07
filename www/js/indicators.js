0b6d075981c7afbd61ea46b2d46c8e32e8ce31b3
Marlo Delatorre <delatorm@purdue.edu>
2016-02-06 22:23:27 -0500

    Added content to login modal.

--- www/js/indicators.js
+++ www/js/indicators.js
@@ -1,44 +1,46 @@
-$('#earhartContainer').radialIndicator({
-        radius: 60,
-        barColor: '#33cc33',
-        barWidth: 10,
-        initValue: 20,
-        roundCorner : false,
-        percentage: true
+$(document).ready(function(){
+    $('#earhartContainer').radialIndicator({
+            radius: 60,
+            barColor: '#33cc33',
+            barWidth: 10,
+            initValue: 20,
+            roundCorner : false,
+            percentage: true
     });
 
-$('#windsorContainer').radialIndicator({
-        radius: 60,
-        barColor: '#33cc33',
-        barWidth: 10,
-        initValue: 20,
-        roundCorner : false,
-        percentage: true
+    $('#windsorContainer').radialIndicator({
+            radius: 60,
+            barColor: '#33cc33',
+            barWidth: 10,
+            initValue: 20,
+            roundCorner : false,
+            percentage: true
     });
 
-$('#wileyContainer').radialIndicator({
-        radius: 60,
-        barColor: '#33cc33',
-        barWidth: 10,
-        initValue: 20,
-        roundCorner : false,
-        percentage: true
+    $('#wileyContainer').radialIndicator({
+            radius: 60,
+            barColor: '#33cc33',
+            barWidth: 10,
+            initValue: 20,
+            roundCorner : false,
+            percentage: true
     });
 
-$('#fordContainer').radialIndicator({
-        radius: 60,
-        barColor: '#33cc33',
-        barWidth: 10,
-        initValue: 20,
-        roundCorner : false,
-        percentage: true
+    $('#fordContainer').radialIndicator({
+            radius: 60,
+            barColor: '#33cc33',
+            barWidth: 10,
+            initValue: 20,
+            roundCorner : false,
+            percentage: true
     });
 
-$('#hillenbrandContainer').radialIndicator({
-        radius: 60,
-        barColor: '#33cc33',
-        barWidth: 10,
-        initValue: 20,
-        roundCorner : false,
-        percentage: true
+    $('#hillenbrandContainer').radialIndicator({
+            radius: 60,
+            barColor: '#33cc33',
+            barWidth: 10,
+            initValue: 20,
+            roundCorner : false,
+            percentage: true
     });
+});
