86c4d6d67298722253444e1317ac85161dc6d282
Marlo Delatorre <delatorm@purdue.edu>
2016-02-11 20:15:54 -0500

    Changes to stylesheet and indicators.

--- www/js/indicators.js
+++ www/js/indicators.js
@@ -1,43 +1,68 @@
 $(document).ready(function(){
     $('#earhartContainer').radialIndicator({
             radius: 60,
-            barColor: '#33cc33',
+            barColor: {
+                0: '#33cc99',
+                33: '#ffdd40',
+                66: '#ffad40',
+                100: '#ff4540'
+            },
             barWidth: 10,
-            initValue: 20,
+            initValue: 100,
             roundCorner : false,
             percentage: true
     });
 
     $('#windsorContainer').radialIndicator({
             radius: 60,
-            barColor: '#33cc33',
+            barColor: {
+                0: '#33cc99',
+                33: '#ffdd40',
+                66: '#ffad40',
+                100: '#ff4540'
+            },
             barWidth: 10,
-            initValue: 20,
+            initValue: 30,
             roundCorner : false,
             percentage: true
     });
 
     $('#wileyContainer').radialIndicator({
             radius: 60,
-            barColor: '#33cc33',
+            barColor: {
+                0: '#33cc99',
+                33: '#ffdd40',
+                66: '#ffad40',
+                100: '#ff4540'
+            },
             barWidth: 10,
-            initValue: 20,
+            initValue: 60,
             roundCorner : false,
             percentage: true
     });
 
     $('#fordContainer').radialIndicator({
             radius: 60,
-            barColor: '#33cc33',
+            barColor: {
+                0: '#33cc99',
+                33: '#ffdd40',
+                66: '#ffad40',
+                100: '#ff4540'
+            },
             barWidth: 10,
-            initValue: 20,
+            initValue: 90,
             roundCorner : false,
             percentage: true
     });
 
     $('#hillenbrandContainer').radialIndicator({
             radius: 60,
-            barColor: '#33cc33',
+            barColor: {
+                0: '#33cc99',
+                33: '#ffdd40',
+                66: '#ffad40',
+                100: '#ff4540'
+            },
             barWidth: 10,
             initValue: 20,
             roundCorner : false,
