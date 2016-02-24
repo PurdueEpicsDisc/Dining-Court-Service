919dcbfd2888b42841fe02ac1b21c37264f8e15a
zhiyanglin <lin382@purdue.edu>
2016-02-24 15:39:09 -0500

    button

--- www/footer.php
+++ www/footer.php
@@ -8,8 +8,8 @@
                     </div>
                     <div class="modal-body">
                         <form class="form-signin">
-                            <label for="inputEmail" class="sr-only">Email address</label>
-                            <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
+                            <label for="inputEmail" class="sr-only">Admin Username</label>
+                            <input type="email" id="inputEmail" class="form-control" placeholder="Admin Username" required autofocus>
                             <label for="inputPassword" class="sr-only">Password</label>
                             <input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
                             <div class="checkbox">
@@ -17,7 +17,7 @@
                                     <input type="checkbox" value="remember-me"> Remember me
                                 </label>
                             </div>
-                            <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+                            <button class="btn btn-lg btn-primary btn-block" id="signinbtn" type="submit">Sign in</button>
                         </form>
                     </div>
                 </div>
