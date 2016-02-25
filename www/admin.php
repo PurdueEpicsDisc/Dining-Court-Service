f6440bbfa4692b111e296bb76b2cbcde490d52a2
zhiyanglin <lin382@purdue.edu>
2016-02-25 01:52:41 -0500

    admin update

--- www/admin.php
+++ www/admin.php
@@ -2,26 +2,42 @@
     <div id="main-content" class="container">
         <table id="admin-table" class="table table-striped">
           <tr>
-            <td>Camera ID</td>
-            <td>Location</td> 
-            <td>Direction</td>
-            <td>Status</td>
-            <td>IP Address</td>
-            <td>MAC Address</td>
+            <td><span>Camera ID</span></td>
+            <td><span>Location</span></td> 
+            <td><span>Direction</span></td>
+            <td><span>Status</span></td>
+            <td><span>IP Address</span></td>
+            <td><span>MAC Address</span></td>
           </tr>
           <tr>
-            <td>1</td>
+            <td class="camera_num">1</td>
             <td>Earhart</td>
             <td>IN</td>
-            <td>ON</td>
+            <td>
+              <div class="onoffswitch">
+                  <input type="checkbox" name="onoffswitch" class="onoffswitch-checkbox" id="num1onoffswitch" checked>
+                  <label class="onoffswitch-label" for="num1onoffswitch">
+                      <span class="onoffswitch-inner"></span>
+                      <span class="onoffswitch-switch"></span>
+                  </label>
+              </div>
+            </td>
             <td>1.1.1.1</td> 
             <td>1.1.1.1</td>
           </tr>
           <tr>
-            <td>2</td>
+            <td>2</td> 
             <td>Earhart</td>
             <td>OUT</td>
-            <td>ON</td>
+            <td>
+              <div class="onoffswitch">
+                  <input type="checkbox" name="onoffswitch" class="onoffswitch-checkbox" id="num2onoffswitch" checked>
+                  <label class="onoffswitch-label" for="num2onoffswitch">
+                      <span class="onoffswitch-inner"></span>
+                      <span class="onoffswitch-switch"></span>
+                  </label>
+              </div>
+            </td>
             <td>1.1.1.1</td> 
             <td>1.1.1.1</td>
           </tr>
@@ -29,7 +45,7 @@
             <td>3</td>
             <td>Wiley</td>
             <td>IN</td>
-            <td>ON</td>
+            <td>OFF</td>
             <td>1.1.1.1</td> 
             <td>1.1.1.1</td>
           </tr>
@@ -37,7 +53,55 @@
             <td>4</td>
             <td>Wiley</td>
             <td>OUT</td>
-            <td>ON</td>
+            <td>OFF</td>
+            <td>1.1.1.1</td> 
+            <td>1.1.1.1</td>
+          </tr>
+          <tr>
+            <td>5</td>
+            <td>Ford</td>
+            <td>IN</td>
+            <td>OFF</td>
+            <td>1.1.1.1</td> 
+            <td>1.1.1.1</td>
+          </tr>
+          <tr>
+            <td>6</td>
+            <td>Ford</td>
+            <td>OUT</td>
+            <td>OFF</td>
+            <td>1.1.1.1</td> 
+            <td>1.1.1.1</td>
+          </tr>
+          <tr>
+            <td>7</td>
+            <td>Windsor</td>
+            <td>IN</td>
+            <td>OFF</td>
+            <td>1.1.1.1</td> 
+            <td>1.1.1.1</td>
+          </tr>
+          <tr>
+            <td>8</td>
+            <td>Windsor</td>
+            <td>OUT</td>
+            <td>OFF</td>
+            <td>1.1.1.1</td> 
+            <td>1.1.1.1</td>
+          </tr>
+          <tr>
+            <td>9</td>
+            <td>Hillenbrand</td>
+            <td>IN</td>
+            <td>OFF</td>
+            <td>1.1.1.1</td> 
+            <td>1.1.1.1</td>
+          </tr>
+          <tr>
+            <td>10</td>
+            <td>Hillenbrand</td>
+            <td>OUT</td>
+            <td>OFF</td>
             <td>1.1.1.1</td> 
             <td>1.1.1.1</td>
           </tr>
