3df00c26d61d7b69637de04d29eb44dc4249641d
Marlo Delatorre <delatorm@purdue.edu>
2016-04-21 02:20:32 -0400

    Added close button on modal

--- www/index.php
+++ www/index.php
@@ -87,6 +87,7 @@
             <div class="modal-dialog" role="document">
                 <div class="modal-content">
                     <div class="modal-header">
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
                         <h3 style="text-align: center;">Earhart Trends</h3>
                     </div>
                     <div class="modal-body" style="text-align: center;">
